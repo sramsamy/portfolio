3a0e1d8d843261308fbdb5b29512c28a78ffa49b
Sachin Ramsamy <sachin_drz125@hotmail.com>
2024-03-26 18:49:55 +1000

    Added links and motion animation to navbar

--- components/header.tsx
+++ components/header.tsx
@@ -2,14 +2,38 @@
 
 import React from 'react';
 import  { motion } from 'framer-motion';
+import { links } from "@/lib/data";
+import Link from 'next/link';
 
 export default function Header() {
     return (
         <header className="z-[999] relative">
-            <motion.div className="fixed top-0 right-0 h-[4.5rem] w-full bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+            <motion.div className="fixed top-0 right-0 h-[4.5rem] w-full bg-slate-950 bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
             initial={{ y: -100, opacity: 0 }}
             animate = {{ y: 0, opacity: 1 }}
-            ></motion.div>
+            >
+            </motion.div>
+
+            <nav className="flex fixed top-0 h-12 py-2 right-0 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+            <ul className='flex w-[22rem flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-blue-700 sm:w-[initial sm:flex-nowrap sm:gap-5 mr-5'>
+                {links.map(link => (
+                    <motion.li 
+                    className='h-3/4 flex items-center justify-center' 
+                    key={link.hash}
+                    initial={{ y: -100, opacity: 0 }}
+                    animate = {{ y: 0, opacity: 1 }}
+                    >
+                        <Link 
+                        className='flex w-full items-center justify-center px-2 py-3 hover:text-blue-900 transition' 
+                        href={link.hash}
+                        >
+                            {link.name}
+                        </Link>    
+                    </motion.li>
+                ))}
+            </ul>
+
+            </nav>
         </header>
     )
 }
